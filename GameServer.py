31f90696ff374e0a84392b1ecdee0d504f8dc935
gpogoncheff <galenpogoncheff@me.com>
2018-03-16 21:53:42 -0600

    Start of game server implementation

---
+++ GameServer.py
@@ -0,0 +1,30 @@
+import socket
+
+class GameServer:
+
+    def __init__(self, address=socket.gethostbyname(''), port=1234):
+        self.address = address
+        self.port = port
+        self.connections = []
+        self.serverSocket = socket.socket()
+        self.serverSocket.bind((self.address, self.port))
+        self.serverSocket.listen()
+
+    def addConnection(self, clientSocket):
+        clientSocket.connect((self.address, self.port))
+        c, addr = self.serverSocket.accept()
+        self.connections.append((clientSocket, c))
+
+    def notify(self):
+        for conns in self.connections:
+            conns[1].send(bytes("You have been connected", 'utf-8'))
+            print(str(conns[0].recv(1024), 'utf-8'))
+
+'''
+server = GameServer()
+client = socket.socket()
+server.addConnection(client)
+server.notify()
+client.close()
+server.serverSocket.close()
+'''
