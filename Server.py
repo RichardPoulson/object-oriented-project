f949b1c38c3712ebd106fdb320a65ab65d89bfc2
gpogoncheff <galenpogoncheff@me.com>
2018-03-17 20:32:31 -0600

    cleaning server class

--- Server.py
+++ Server.py
@@ -3,13 +3,7 @@ import socket
 
 class Server(metaclass=ABCMeta):
 
-    def __init__(self, address, port):
-        self.address = address
-        self.port = port
-        self.connections = []
-        self.serverSocket = socket.socket()
-        self.serverSocket.bind((self.address, self.port))
-        self.serverSocket.listen()
+    connections = []
 
     def addConnection(self, clientSocket):
         clientSocket.connect((self.address, self.port))
@@ -23,19 +17,4 @@ class Server(metaclass=ABCMeta):
 
     @abstractmethod
     def notify(self):
-        '''
-        for clientSocket, clientConnection in self.connections:
-            clientConnection.send(bytes("You have been connected", 'utf-8'))
-            print(str(clientSocket.recv(1024), 'utf-8'))
-        '''
         pass
-
-'''
-# Testing
-server = GameServer(socket.gethostbyname(''), 1234)
-client = socket.socket()
-server.addConnection(client)
-server.notify()
-client.close()
-server.serverSocket.close()
-'''
