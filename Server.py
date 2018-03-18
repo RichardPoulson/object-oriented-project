c9744f494055cf49f2001f781dc63d3eefef0009
gpogoncheff <galenpogoncheff@me.com>
2018-03-17 20:29:02 -0600

    Making Server class abstract for the purpose of implementing Observer pattern

--- Server.py
+++ Server.py
@@ -1,6 +1,8 @@
-from Server import *
+from abc import ABCMeta, abstractmethod
+import socket
 
-class GameServer(Server):
+class Server(metaclass=ABCMeta):
+
     def __init__(self, address, port):
         self.address = address
         self.port = port
@@ -9,14 +11,31 @@ class GameServer(Server):
         self.serverSocket.bind((self.address, self.port))
         self.serverSocket.listen()
 
+    def addConnection(self, clientSocket):
+        clientSocket.connect((self.address, self.port))
+        c, addr = self.serverSocket.accept()
+        self.connections.append((clientSocket, c))
+
+    def removeConnection(self, clientSocket):
+        for socket, connection, i in enumerate(self.connections):
+            if (clientSocket == socket):
+                self.connections.pop(i)
+
+    @abstractmethod
     def notify(self):
+        '''
         for clientSocket, clientConnection in self.connections:
             clientConnection.send(bytes("You have been connected", 'utf-8'))
             print(str(clientSocket.recv(1024), 'utf-8'))
+        '''
+        pass
 
+'''
+# Testing
 server = GameServer(socket.gethostbyname(''), 1234)
 client = socket.socket()
 server.addConnection(client)
 server.notify()
 client.close()
 server.serverSocket.close()
+'''
