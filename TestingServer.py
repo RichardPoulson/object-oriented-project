fd4f79d44083463c4e5e1e12ead2460650c5ea99
gpogoncheff <galenpogoncheff@me.com>
2018-03-19 15:26:29 -0600

    client/server updates

---
+++ TestingServer.py
@@ -0,0 +1,22 @@
+# Testing
+from Server import *
+from ClientSocket import *
+from CheckersBoard import *
+
+import time
+
+server = Server(socket.gethostbyname(''), 10000, CheckersBoard())
+server.run()
+c = ClientSocket(socket.gethostbyname(''), 10000, 1)
+c2 = ClientSocket(socket.gethostbyname(''), 10000, 2)
+time.sleep(1)
+c.sendMessage(input("Message 1: "))
+c.receiveMessage()
+c2.receiveMessage()
+c2.sendMessage(input("Message 2: "))
+c.receiveMessage()
+c2.receiveMessage()
+
+c.clientSocket.close()
+c2.clientSocket.close()
+server.serverSocket.close()
