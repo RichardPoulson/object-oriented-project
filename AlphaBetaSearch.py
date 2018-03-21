e97f4dac96129d59219753cfea5f6df7bc1fe990
Richard Poulson <rick.poulson@colorado.edu>
2018-03-21 10:14:25 -0600

    Currently re-writing AlphaBetaSearch
    Defining it to work with "states" instead of MinMaxNodes

--- AlphaBetaSearch.py
+++ AlphaBetaSearch.py
@@ -10,39 +10,57 @@ https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
 """
 
 from sys import maxsize # maxsize used to compare other node values
-from copy import copy # to make copies of alpha and beta
+# from copy import copy # to make copies of alpha and beta
 
 class AlphaBetaSeach:
-  def search(self, node):
+  # at this point, the class has no attributes
+  def __init__(self):
+    pass
+  def search(self, state, max_num_moves):
+    # hasn't made any moves yet, so set to 0
+    initial_num_moves = 0
     # Find the action that will lead to the state with the highest possible
     # value.
-    highest_value = self.maxValue(node, -maxsize-1, maxsize)
-    return node[highest_value]
-  def maxValue(self, node, alpha, beta):
-    if (not node.hasChildren()):    # does this node not need to be evaluated?
-      return node.utilityValue()
-    current_value = -maxsize - 1    # assign lowest possible value to val
-    current_alpha = copy(alpha)
-    for child in node:
-      temp_value = self.minValue(child, current_alpha, beta)  # get the
-      # value and evaluation count from eachChild
-      current_value = max(current_value, temp_value)  # update val to the new maximum value
-      if (current_value >= beta):  # if this child's value is bigger than val..
-        return current_value  # ..Min won't choose 
-      current_alpha = max(current_alpha, current_value)
-    # found the value for MinMaxNode, so set value
-    node.setValue(current_value)
+    highest_value = self.maxValue(state, -maxsize-1, maxsize, initial_num_moves, max_num_moves)
+    return state[highest_value]
+  def maxValue(self, state, alpha, beta, moves_so_far, max_num_moves):
+    if (moves_so_far >= max_num_moves): # have we looked far enough ahead?
+      return state.utilityValue() # if so, get the utility value for this state
+    current_value = -maxsize - 1 # lowest possible value
+    # current_alpha = copy(alpha) # make a copy so it doesn't reference original
+    # current_num_moves = copy(moves_so_far) # ""
+    for following_state in state: # for every possible action/move in this state:
+      # find the move with the lowest value, since this would be the opposing
+      # player's best move
+      temp_value = self.minValue(following_state, alpha, beta, moves_so_far, max_num_moves)
+      # out of all the possible states made by the opposing player's moves,
+      # choose the one with the highest value, since this would benefit the
+      # computer player
+      current_value = max(current_value, temp_value)
+      # if condition true, opposing player probably won't choose a move on this
+      # branch, so simply return the current value without checking any other
+      # game states
+      if (current_value >= beta):
+        return current_value
+      # set the current upper-bound value for this game state
+      alpha = max(alpha, current_value)
+    # found the value for the game state, so set state value and return value
+    state.setValue(current_value)
     return current_value
-  def minValue(self, node, alpha, beta):
-    if (not node.hasChildren()):    # does this node not need to be evaluated?
-      return node.utilityValue()
-    current_beta = copy(beta)
-    current_value = maxsize
-    for child in node:
-      temporary_value = self.maxValue(child, alpha, current_beta)
-      current_value = min(current_value, temporary_value)
+  def minValue(self, state, alpha, beta, moves_so_far, max_num_moves):
+    if (moves_so_far >= max_num_moves): # have we looked far enough ahead?
+      return state.utilityValue() # if so, get the utility value for this state
+    current_value = maxsize # maximum size possible
+    # current_beta = copy(beta)
+    for following_state in state:
+      temp_value = self.maxValue(following_state, alpha, beta, moves_so_far, max_num_moves)
+      current_value = min(current_value, temp_value)
+      # if condition true, don't want to choose this path, since it will most
+      # likely end up with a very low utility value.  Simply return value
+      # without checking any more states
       if (current_value <= alpha):
         return current_value
-      current_beta = min(current_beta, current_value)
-    node.setValue(current_value)
+      # set beta to the lowest value found so fare
+      beta = min(beta, current_value)
+    state.setValue(current_value)
     return current_value
