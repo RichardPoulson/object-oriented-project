1a8072dcd22262306fc7967c127566cf83a2b670
Richard Poulson <rick.poulson@colorado.edu>
2018-03-30 21:08:01 -0600

    made some updates to GameState
    set up GameState to use setters in the constructor instead of the assignment instructions.  Working on setting up an Iterator for possible piece movements.

--- GameState.py
+++ GameState.py
@@ -7,36 +7,36 @@ Defines GameState, which defines the state of a checkers game.
 """
     for AIplayer, on update(), possibly get newGameBoard, then gamestate.set()
 """
-from copy import deepcopy
+from copy import copy, deepcopy
 
 class GameState:
-  # Constructor
-  def __init__(self, spaces, currentPlayer, lastMove):
-    self.spaces = deepcopy(spaces) # copy objects instead of references
+  def __init__(self, checkersBoard, currentPlayer, lastMove):
+    self.setCheckersBoard(checkersBoard)
+    self.setSpaces(self.getCheckersBoard().spaces)
+    self.setCurrentPlayer(currentPlayer)
+    self.setLastMove(lastMove)
+    self.possibleMoves = None
+  def setCheckersBoard(self, newCheckersBoard):
+    self.checkersBoard = newCheckersBoard
+  def getCheckersBoard(self, newCheckersBoard):
+    self.checkersBoard = newCheckersBoard
+  def setSpaces(self, spaces):
+    self.spaces = copy(spaces) # copies object instead of reference
+  def getSpaces(self):
+    return self.spaces
+  def setCurrentPlayer(self, currentPlayer):
     self.currentPlayer = currentPlayer
-    self.lastMove = deepcopy(lastMove)
-  def setCurrentPlayer(self, currentPlayer): self.currentPlayer = currentPlayer
-  def getCurrentPlayer(self): return self.currentPlayer
-  def setSpaces(self, spaces): self.spaces = deepcopy(spaces)
-  def getSpaces(self): return self.spaces
-  def setLastMove(self, lastMove): self.lastMove = deepcopy(lastMove)
-  def getLastMove(self): return self.lastMove
+  def getCurrentPlayer(self):
+    return self.currentPlayer
+  def setLastMove(self, lastMove):
+    self.lastMove = lastMove
+  def getLastMove(self):
+    return self.lastMove
   def __iter__(self):
     return iter(self.possible_moves)
-  # used to determine if this MinMaxNode is == to another
-  def __eq__(self, other):
-    return (self.value == other.value)
-  # used to determine if this MinMaxNode is < another
-  def __lt__(self, other):
-    return (self.value < other.value)
-  def __getitem__(self, key):
-    for child in self:
-      if child.getValue() == key:
-        return child
-
   def getAvailableMoves(self):
-    for player in self.game_board.observers:
+    for player in self.getCheckersBoard().observers:
         for piece in player.getPlayerPieces():
             for moveType in ['moveLeft', 'moveRight', 'jumpLeft', 'jumpRight']:
-                if self.game_board.isValidMove(player, piece.getLocation(), moveType):
+                if self.getCheckersBoard().isValidMove(player, piece.getLocation(), moveType):
                     self.possible_moves.append((piece, moveType))
